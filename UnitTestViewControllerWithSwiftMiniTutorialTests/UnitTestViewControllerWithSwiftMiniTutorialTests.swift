941bbecf1f568ed75161fd0d4cabaabc4c21a5fa
SnappedCoffee <snapped@ymail.com>
2015-01-02 17:03:53 +0100

    add unit tests incl. setUp()

--- UnitTestViewControllerWithSwiftMiniTutorialTests/UnitTestViewControllerWithSwiftMiniTutorialTests.swift
+++ UnitTestViewControllerWithSwiftMiniTutorialTests/UnitTestViewControllerWithSwiftMiniTutorialTests.swift
@@ -14,9 +14,22 @@ import UnitTestViewControllerWithSwiftMiniTutorial
 
 class UnitTestViewControllerWithSwiftMiniTutorialTests: XCTestCase {
     
+    // Important: create a variable for the view controller outside of setup(). Otherwise the variable is not available to the test functions
+    var vc:ViewController = ViewController()
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        /* Set up the storyboard with the view controller.
+        Source: http://www.iosmike.com/2014/08/unit-testing-viewcontrollers-in-swift.html
+        Important: set the Storyboard ID to the same value as in instantiateViewControllerWithIdentifier() (e.g. MyStoryboard). Here is how: open the storyboard, select the view controller in the storyboard, select the identity inspector tab in the right inspector and finally set the Storyboard ID property in the right inspector)
+        */
+        
+        var storyboard:UIStoryboard = UIStoryboard(name: "Main", bundle: NSBundle(forClass: self.dynamicType))
+        vc = storyboard.instantiateViewControllerWithIdentifier("MyStoryboard") as ViewController
+        vc.loadView()
+        vc.viewDidLoad()
+        
     }
     
     override func tearDown() {
@@ -24,16 +37,20 @@ class UnitTestViewControllerWithSwiftMiniTutorialTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+    func testViewDidLoad() {
+        // assert that the ViewController.view is not nil
+        XCTAssertNotNil(vc.view, "View Did Not load")
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
+    func testMyLabelTextDidLoad() {
+        // assert that myLabel was set to "Hello World" when viewDidLoad
+        XCTAssertTrue(vc.myLabel.text == "Hello World", "myLabel is not set to 'Hello World', but to \(vc.myLabel.text)")
+    }
+    
+    func testMyButtonPressedChangesMyLabel() {
+        // assert that myLabel is set to "Button has been pressed", when myButton has been pressed
+        vc.myButtonPressed(vc.myButton)
+        XCTAssertTrue(vc.myLabel.text == "Button has been pressed", "myLabel is not set to 'Button has been pressed', but to \(vc.myLabel.text)")
     }
     
 }
